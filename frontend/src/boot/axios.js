d140737f7e8be31ff4960e6c88d65635258f21bc
Rafael Oliveira <rafael-goncalves-oliveira@outlook.com>
2020-09-18 23:39:40 -0300

    Deslogando usuário em 403

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,12 +1,29 @@
-import Vue from 'vue'
 import axios from 'axios'
+import { Notify } from 'quasar'
 
 axios.defaults.baseURL = 'http://localhost:8080/'
-axios.interceptors.request.use(function (config) {
-  config.withCredentials=  true
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-})
 
-Vue.prototype.$axios = axios
+
+export default  ({ Vue, router }) => {
+  Vue.prototype.$axios = axios
+  axios.interceptors.request.use(function (config) {
+    config.withCredentials=  true
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
+  })
+  
+  axios.interceptors.response.use(function (response) {
+    return response;
+  }, function (error) {
+    if (error.response.status == 403) {
+      Notify.create({
+        message: 'Usuário desconectado!',
+        type: 'negative',
+        icon: 'error'
+      })
+      router.push('/login')
+    }
+    return Promise.reject(error);
+  })
+}
