ab0558a024302a4eff9f3cce2d685ae4bd8bd04b
gabrielapaixao <42282926+gabrielapaixao@users.noreply.github.com>
2020-09-21 13:52:08 -0300

    Add files via upload

---
+++ frontend/src/TodoItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-item tag="label" v-ripple class="q-my-sm bg-white">
+    <q-item-section avatar>
+      <q-checkbox v-model="done" :color="priority_color" keep-color>
+        <q-tooltip>
+          <div v-if="done">
+            Concluído em {{ conclusion_date }}
+          </div>
+          <div v-else>
+            Marcar como concluído
+          </div>
+        </q-tooltip>
+      </q-checkbox>
+    </q-item-section>
+    <q-item-section>
+      <q-item-label :class="done ? 'text-strike text-grey-8' : null">{{ name }}</q-item-label>
+      <q-item-label caption :class="done ? 'text-strike' : null">{{ description }}</q-item-label>
+    </q-item-section>
+    <q-item-section side>
+      <div class="row no-wrap">
+        <q-btn color="black" flat round icon="edit" @click="edit">
+          <q-tooltip>
+            Editar tarefa
+          </q-tooltip>
+        </q-btn>
+        <q-btn color="black" flat round icon="delete_forever" @click="remove">
+          <q-tooltip>
+            Remover tarefa
+          </q-tooltip>
+        </q-btn>
+      </div>
+    </q-item-section>
+    <q-badge color="black" floating v-if="!done && days_left < 0">
+      {{ Math.abs(days_left) }}
+      <q-tooltip>
+        Dias em atraso
+      </q-tooltip>
+    </q-badge>
+  </q-item>
+</template>
+
+<style>
+
+</style>
